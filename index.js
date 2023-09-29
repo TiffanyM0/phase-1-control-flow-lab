16fac7646d5625253394b7084167cb668c06ec10
TIFFANY MAINA <tiffmaina@gmail.com>
2023-09-29 21:52:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
   // Write your code here!
+// function scuberGreetingForFeet(number){
+//   let result; 
+//   if (scuberGreetingForFeet <= 400){
+//     result = 'This one is on me!';
+//   } 
+//   else {
+//     result = 'I will gladly take your thirty bucks.'
+//   }
+  function scuberGreetingForFeet(amount) {
+    if (amount < 400) {
+      return 'This one is on me!';
+    } else if (amount > 2500){
+      return 'No can do.'
+    }
+    else {
+      return 'I will gladly take your thirty bucks.'
+    }
+  }
+
+
+  // Write your code here!
+
+
+function ternaryCheckCity(place){
+  return place === 'NYC'? 'Ok, sounds good.': 'No go.'  
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
